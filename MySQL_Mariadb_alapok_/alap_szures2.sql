41a96a3cc02d38c84369611c38d5877349b0c02d
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2025-01-12 19:07:06 +0100

    Update alap_szures2.sql

--- MySQL_Mariadb_alapok_/alap_szures2.sql
+++ MySQL_Mariadb_alapok_/alap_szures2.sql
@@ -12,3 +12,32 @@ SELECT CategoryName FROM Categories;
 
 SELECT CategoryName,Descriptio FROM Categories;
 
+/*2. Feladat
+Írj SQL utasítást amely az online tesztadatbázis 'Products' táblájából lekérdezi
+a, a 'ProductName', 'Unit' és 'Price' oszlopba tartozó adatokat,
+b, azon termékek nevét és árát, amelyek ára 20-nál kevesebb,
+c, azon termékek nevét és árát, amelyek ára 20 és 30 közé esik,
+d, azon termékek nevét és árát, amelyek ára nem 10,
+e, a 'T' betűvel kezdődő temékek nevét,
+f, azon termékek nevét, amelynek neve a 'Louisiana' szóval kezdődik!*/
+
+SELECT * FROM Products;
+
+SELECT ProductName,Unit,Price FROM Products;
+
+SELECT ProductName,Price FROM Products WHERE Price < 20;
+
+SELECT ProductName,Price FROM Products WHERE Price BETWEEN 20 AND 30;
+
+SELECT ProductName,Price FROM Products WHERE 20 < Price AND Price < 30;
+
+SELECT ProductName,Price FROM Products WHERE Price <> 10;
+
+SELECT ProductName,Price FROM Products WHERE ProductName LIKE 'T%';
+
+SELECT ProductName FROM Products WHERE ProductName LIKE 'Louisiana%';
+
+/*3. Feladat
+Írj SQL utasítást amely az online tesztadatbázis 'Suppliers' táblájából lekérdezi az országokat ismétlődés nélkül!*/
+
+SELECT DISTINCT Country FROM Suppliers;
