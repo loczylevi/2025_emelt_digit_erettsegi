2959d85bf6711bf367932fb24017d866094d66d2
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2025-01-15 18:17:59 +0100

    Create CREATE.sql

---
+++ MySQL_Mariadb_alapok_/CREATE.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers(last_name,first_name,company,city,email_address) VALUES("Gipsz", "Jakab", "Bezsonet","veszprém","loczxy2003@gmail.com");
+
+
+CREATE DATABASE muhely;
+DROP DATABASE muhely;
