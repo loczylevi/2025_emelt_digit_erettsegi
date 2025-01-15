b9e3f40fe54d435d763673cae45abe6bbb0fdf02
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2025-01-15 19:14:49 +0100

    Update create_database.sql

--- MySQL_Mariadb_alapok_/create_database.sql
+++ MySQL_Mariadb_alapok_/create_database.sql
@@ -3,12 +3,15 @@ CREATE DATABASE muhely;
 USE muhely;
 
 CREATE TABLE szgk(
-    id INT,
-    tipus VARCHAR(20),
-    modell VARCHAR(20),
-    rendszam VARCHAR(20),
-    ajtok_szama INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    tipus VARCHAR(20) DEFAULT "Toyota",
+    modell VARCHAR(20) NOT NULL,
+    rendszam VARCHAR(20) UNIQUE,
+    ajtok_szama INT CHECK(ajtok_szama < 6),
     gyartas_eve DATE
 );
 
 INSERT INTO szgk(tipus,modell,rendszam,ajtok_szama, gyartas_eve) VALUES("Skoda", "Opel","LOL345",4,"1972-04-30");
+INSERT INTO szgk(tipus,modell,rendszam,ajtok_szama, gyartas_eve) VALUES("Skoda", "Opel","LOL345",4,"1972-04-30");
+
+SELECT * FROM szgk;
