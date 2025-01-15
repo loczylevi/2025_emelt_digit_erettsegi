d86e43143157cc72116e7bfec39efc45d3cc1a83
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2025-01-15 18:58:07 +0100

    Update create_database.sql

--- MySQL_Mariadb_alapok_/create_database.sql
+++ MySQL_Mariadb_alapok_/create_database.sql
@@ -1,3 +1,14 @@
 DROP DATABASE IF EXISTS muhely;
 CREATE DATABASE muhely;
 USE muhely;
+
+CREATE TABLE szgk(
+    id INT,
+    tipus VARCHAR(20),
+    modell VARCHAR(20),
+    rendszam VARCHAR(20),
+    ajtok_szama INT,
+    gyartas_eve DATE
+);
+
+INSERT INTO szgk(tipus,modell,rendszam,ajtok_szama, gyartas_eve) VALUES("Skoda", "Opel","LOL345",4,"1972-04-30");
