cfa9b4a2de7a0d3e7418c0cad95e7c0195f02d35
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2025-01-15 18:27:42 +0100

    Update CREATE.sql

--- MySQL_Mariadb_alapok_/CREATE.sql
+++ MySQL_Mariadb_alapok_/CREATE.sql
@@ -1,5 +1,8 @@
 INSERT INTO customers(last_name,first_name,company,city,email_address) VALUES("Gipsz", "Jakab", "Bezsonet","veszprém","loczxy2003@gmail.com");
 
 SELECT * FROM `customers` WHERE city = "Veszprém";
+
+UPDATE customers SET last_name = "Bedecsi", company = "Bezsonet" WHERE id = 1;
+
 CREATE DATABASE muhely;
 DROP DATABASE muhely;
